ca0c73f89a860b6410579ef0fb26bc870c1ef12b
Yash <58439868+yash-markad@users.noreply.github.com>
2022-08-09 22:00:14 +0530

    Update xbox_vibration.py

--- xbox_vibration.py
+++ xbox_vibration.py
@@ -1,5 +1,5 @@
 import inputs
 
-gamepd = inputs.devices.gamepad()[0]
+gamepd = inputs.devices.gamepads[0]
 
 gamepd.set_vibration(0.6,0.8,100) #(vibration sensitivity 0-1) (0-1 left, 0-1 right, time_duration in ms)
